7c2bcaf286c1cf6469e6b2b23532bf31b9818f96
Umut Topuzoğlu <uloco@schiefewelt.de>
2019-05-09 23:44:45 +0200

    Add particles as bg

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
+import Particles from "react-particles-js";
 import "./App.css";
+import particlesConfig from "./assets/particles.json";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles params={particlesConfig} />
     </div>
   );
 };
