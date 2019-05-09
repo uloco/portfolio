f2795ee6117898827cd8e8294a384005b4d75b7c
Umut Topuzoğlu <uloco@schiefewelt.de>
2019-05-10 00:01:47 +0200

    Resize canvas on window resize

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from "react";
 import Particles from "react-particles-js";
 import "./App.css";
-import particlesConfig from "./assets/particles.json";
+import particlesConfig from "./assets/particlesConfig.json";
 
-const App = () => {
-  return (
-    <div className="App">
-      <Particles params={particlesConfig} />
-    </div>
-  );
-};
+class App extends React.Component {
+  state = { width: window.innerWidth, height: window.innerHeight };
+
+  componentWillMount() {
+    this.updateDimensions();
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
+  updateDimensions = () => {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  };
+
+  render() {
+    const { width, height } = this.state;
+    return (
+      <div className="App">
+        <Particles params={particlesConfig} width={width} height={height} />
+        <div className="App-content">Content will render here</div>
+      </div>
+    );
+  }
+}
 
 export default App;
