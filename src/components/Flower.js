fad5b3bf45d2ceddd25b40ac43107d862ee7dfb4
Umut Topuzoğlu <uloco@schiefewelt.de>
2019-10-24 22:37:07 +0200

    Add centered circle

--- src/components/Flower.js
+++ src/components/Flower.js
@@ -1,28 +1,37 @@
 import React from "react";
-import { animated, useSpring } from "react-spring";
+import { animated, config, useSpring } from "react-spring";
 import styled from "styled-components/macro";
 
 const Circle = styled(animated.div)`
-  display: flex;
   background-color: ${props => props.color || "white"};
   border-radius: 50%;
-  width: 200px;
-  height: 200px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: -50px 0 0 -50px;
+  width: 100px;
+  height: 100px;
   color: black;
+  display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 const Flower = props => {
-  const style = useSpring({ opacity: 1, from: { opacity: 0 } });
+  const style = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: config.gentle
+  });
+
   return (
     <div>
       <Circle style={style}>That's me!</Circle>
-      <Circle>Arts</Circle>
+      {/* <Circle>Arts</Circle>
       <Circle>Sounds</Circle>
       <Circle>Thoughts</Circle>
       <Circle>Reads</Circle>
-      <Circle>Bits</Circle>
+      <Circle>Bits</Circle> */}
     </div>
   );
 };
