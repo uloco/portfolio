d52ba2080c86ee26a181906dde07347f82a7d162
Umut Topuzoğlu <uloco@schiefewelt.de>
2019-11-16 13:11:00 +0100

    Refactor App to FP style

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import React from "react";
+import _ from "lodash";
+import React, { useEffect, useState } from "react";
 import Particles from "react-particles-js";
 import "./App.css";
 import Flower from "./components/Flower";
 import particlesConfig from "./particlesConfig.json";
 
-class App extends React.Component {
-  state = { width: window.innerWidth, height: window.innerHeight };
-  resizeTimer;
+const App = () => {
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
 
-  componentDidMount() {
-    window.addEventListener("resize", this.updateDimensions);
-  }
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
 
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateDimensions);
-  }
-
-  updateDimensions = () => {
-    clearTimeout(this.resizeTimer);
-    this.resizeTimer = setTimeout(() => {
-      this.setState({ width: window.innerWidth, height: window.innerHeight });
-    }, 150);
+  const handleResize = () => {
+    _.debounce(
+      () =>
+        setDimensions({ width: window.innerWidth, height: window.innerHeight }),
+      150
+    );
   };
 
-  render() {
-    const { width, height } = this.state;
-    return (
-      <div>
-        <div className="Particles">
-          <Particles params={particlesConfig} width={width} height={height} />
-        </div>
-        <div className="App">
-          <Flower />
-        </div>
+  const { width, height } = dimensions;
+
+  return (
+    <div>
+      <div className="Particles">
+        <Particles params={particlesConfig} width={width} height={height} />
       </div>
-    );
-  }
-}
+      <div className="App">
+        <Flower />
+      </div>
+    </div>
+  );
+};
 
 export default App;
