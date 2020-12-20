9deffa9a0606fcdb8e5e3980cb86d5dfb590b5f8
Umut Topuzoğlu <uloco@schiefewelt.de>
2020-12-20 14:43:43 +0100

    Fix Images

--- src/components/Flower.js
+++ src/components/Flower.js
@@ -3,19 +3,18 @@ import React from "react";
 import { animated, useSpring, useSprings } from "react-spring";
 import arts from "../assets/arts.svg";
 import bits from "../assets/bits.svg";
-import read from "../assets/read.svg";
+import books from "../assets/books.svg";
 import sounds from "../assets/sounds.svg";
 import thoughts from "../assets/thoughts.svg";
+import avatar from "../assets/avatar.jpeg";
+
 import { useWindowSize } from "../hooks/useWindowSize";
 import Circle from "./Circle";
 
 const AnimatedCircle = animated(Circle);
-
 const maxCircleRadius = 150;
-const gravatarUrl = `https://www.gravatar.com/avatar/
-c5d5195acc9f791ee59a1f9eeb2bad57?s=${maxCircleRadius}`;
 
-const Flower = props => {
+const Flower = (props) => {
   const { width } = useWindowSize();
 
   const circleRadius = _.clamp(Math.round(width * 0.125), 75, maxCircleRadius);
@@ -25,8 +24,8 @@ const Flower = props => {
     { text: "arts", image: arts },
     { text: "sounds", image: sounds },
     { text: "thoughts", image: thoughts },
-    { text: "read", image: read },
-    { text: "bits", image: bits }
+    { text: "books", image: books },
+    { text: "bits", image: bits },
   ];
   const avatarStyle = useSpring({ opacity: 1, from: { opacity: 0 } });
 
@@ -40,20 +39,21 @@ const Flower = props => {
       return {
         opacity: 1,
         transform: `translate(${x}px,${y}px)`,
-        from: { opacity: 0, transform: "translate(0px,0px)" }
+        from: { opacity: 0, transform: "translate(0px,0px)" },
       };
     })
   );
 
   const circles = circleData.map((data, i) => ({
     ...data,
-    spring: springs[i]
+    spring: springs[i],
   }));
 
   return (
     <div>
       <AnimatedCircle
-        src={gravatarUrl}
+        src={avatar}
+        alt={"avatar"}
         size={circleRadius}
         style={avatarStyle}
       />
